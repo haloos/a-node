53eb343d92c88c3f7df961c5e637c52f5030c34d
Jason <haloos@me.com>
2019-08-27 16:34:02 -0400

    created unlink

--- script.js
+++ script.js
@@ -19,9 +19,17 @@ console.log('Sync', file.toString());
 // })  
 
 // Write
-fs.writeFile('bye.txt', 'Sad to see you go!', err => {
- if(err) {
-   console.log(err)
- }
+// fs.writeFile('bye.txt', 'Sad to see you go!', err => {
+ // if(err) {
+   // console.log(err)
+ // }
+// })  
+
+// Delete 
+fs.unlink('./bye.txt', err => {
+  if (err) {
+    console.log(err) 
+  } 
+  console.log('Inception');
 })
-  
+   
