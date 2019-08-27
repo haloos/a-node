05f3c4ae002646812af5835cbba62bdf9b25cdeb
Jason <haloos@me.com>
2019-08-27 16:18:42 -0400

    updated console.log

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ fs.readFile('./hello.txt', (err, data) => {
   if (err) {
     console.log('errrrrrroooooorrrr');
   } 
-  console.log(data.toString('utf8'));
+  console.log('Async',data.toString('utf8'));
 })  
 
 
 const file = fs.readFileSync('./hello.txt'); 
-console.log(file);
+console.log('Sync', file.toString());
