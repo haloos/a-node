a7c3c4a0f8939d2e86af7c718b21c23359ce5e65
Jason <haloos@me.com>
2019-08-27 16:13:15 -0400

    created readFileSync

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ fs.readFile('./hello.txt', (err, data) => {
   if (err) {
     console.log('errrrrrroooooorrrr');
   } 
-  console.log(data.toString());
-})
+  console.log(data.toString('utf8'));
+})  
+
+
+const file = fs.readFileSync('./hello.txt'); 
+console.log(file);
